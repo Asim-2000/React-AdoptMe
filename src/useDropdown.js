530b28f81acee3caecc18cf97b272195154ea52a
Asim <mkhaskheli.bese18seecs@seecs.edu.pk>
2021-01-11 21:44:00 +0500

    added useEffect for breed selection

--- src/useDropdown.js
+++ src/useDropdown.js
@@ -11,9 +11,9 @@ const useDropdown = (label, defaultState, options) => {
         value={state}
         onChange={(e) => setState(e.target.value)}
         onBlur={(e) => setState(e.target.value)}
-        disabled={!options.length}
+        disabled={options.length === 0}
       >
-        <option />
+        <option>All</option>
         {options.map((item) => (
           <option key={item} value={item}>
             {item}
@@ -22,7 +22,7 @@ const useDropdown = (label, defaultState, options) => {
       </select>
     </label>
   );
-  return [state, Dropdown];
+  return [state, Dropdown,setState];
 };
 
 export default useDropdown;
